5b116b71c3c3ffa5b1169fe5c02c43808dd41941
alumno <alumno@Instituto.local>
2025-05-21 08:19:37 -0300

    21/05/25

---
+++ script.js
@@ -0,0 +1,6 @@
+let frutas = ['pera', 'manzana', 'naranja', 'durazno']
+
+let listaFrutas = ''
+for (let indice = 0; indice < frutas.length; indice++) {
+    listaFrutas = listaFrutas + frutas[indice]
+}
